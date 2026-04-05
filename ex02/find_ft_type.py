7c2308f9ba8c1424248495f5544a3a0c87563569
yel-mrab <yahya181elmrabti@gmail.com>
2026-04-05 22:05:36 +0100

    fix: removing the int type check

--- ex02/find_ft_type.py
+++ ex02/find_ft_type.py
@@ -10,8 +10,6 @@ def all_thing_is_obj(object: any) -> int:
         print(f"Dict : {obj_type}")
     elif obj_type == str:
         print(f"{object} is in the kitchen : {obj_type}")
-    elif obj_type == int:
-        print("Type not found")
     else:
-        print(f"{obj_type.__name__.capitalize()} : {obj_type}")
+        print("Type not found")
     return 42
